e64572afafbb244eee0a9479eeafd1475647a782
romanbond <romanjbond@gmail.com>
2016-07-11 15:41:46 -0400

    Calculator Complete

--- inputcal/calc.js
+++ inputcal/calc.js
@@ -1,6 +1,27 @@
-//Triangle
-//romanbond
+var num1, num2, num3, operator;
 
-function myFunction() {
-    alert("hello roman");
+function evaluateId(){
+    num1 = parseInt(document.getElementById("numEntry1").value);
+    num2 = parseInt(document.getElementById("numEntry2").value);
+
+    var operator = document.getElementsByName('operatorRadio');
+    var op_value;
+    for(var i = 0; i < operator.length; i++){
+        if(operator[i].checked){
+            op_value = operator[i].value;
+        }
+    }
+    if(op_value == "plus")
+        num3 = num1+num2;
+    if(op_value == "minus")
+        num3 = num1-num2;
+    if(op_value== "multi")
+        num3 = num1*num2;
+    if(op_value== "divide")
+        num3 = num1/num2;
+   alert(num3);
+    document.getElementById("result").value = num3;
+    num1 = num3;
+    num3 = 0;
+    document.getElementById("numEntry1").value = num1;
 }
