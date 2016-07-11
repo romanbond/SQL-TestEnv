fabcf33b756865f1fb2b61d583b14e769338ebe1
romanbond <romanjbond@gmail.com>
2016-07-11 16:53:59 -0400

    Added Selection

--- inputcal/calc.js
+++ inputcal/calc.js
@@ -5,9 +5,17 @@ function evaluateId(){
     num2 = parseInt(document.getElementById("numEntry2").value);
 
     var operator = document.getElementsByName('operatorRadio');
+	//if(op_value == "plus")
+	//	num3 = num1+num2;
+
+	var op_value = operator.value;
+
+
+	alert(operator)
+	/*
     var op_value;
     for(var i = 0; i < operator.length; i++){
-        if(operator[i].checked){
+        if(operator[i].selected){
             op_value = operator[i].value;
         }
     }
@@ -19,7 +27,9 @@ function evaluateId(){
         num3 = num1*num2;
     if(op_value== "divide")
         num3 = num1/num2;
+
    alert(num3);
+   */
     document.getElementById("result").value = num3;
     num1 = num3;
     num3 = 0;
