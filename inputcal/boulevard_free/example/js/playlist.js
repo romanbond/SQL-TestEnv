0528f3b1f83eaf0ad2599bc0d5be47c21706e3a6
romanbond <romanjbond@gmail.com>
2016-07-20 13:50:29 -0400

    Added BV

---
+++ inputcal/boulevard_free/example/js/playlist.js
@@ -0,0 +1,18 @@
+var myPlaylist = [
+    {
+        mp3:'sample-playlists/1.mp3',
+        duration:'0:30',
+        cover:'sample-playlists/covers/1.jpg',
+        title:'Song Title',
+        artist:'Sample',
+        background:'sample-playlists/bgs/1.jpg'
+    },
+    {
+        mp3:'sample-playlists/2.mp3',
+        duration:'0:30',
+        cover:'sample-playlists/covers/2.jpg',
+        title:'Song Title',
+        artist:'Sample',
+        background:'sample-playlists/bgs/2.jpg'
+    }
+];
